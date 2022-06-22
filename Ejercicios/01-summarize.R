0fc39bc25e58b15679addabc7f24582940248d9c
Cesar Castillo <cesarzc95@gmail.com>
2022-06-22 13:12:06 -0500

    Commit inicial

---
+++ Ejercicios/01-summarize.R
@@ -0,0 +1,11 @@
+summary(data)
+str(data)
+
+summary(data$cylinders)
+summary(data$mpg)
+str(data$cylinders)
+
+
+install.packages(c("modeest","raster","moments"))
+library(modeest)#moda
+library(raster)#
